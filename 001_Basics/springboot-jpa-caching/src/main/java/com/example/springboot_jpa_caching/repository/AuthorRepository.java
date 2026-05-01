965545c3acfad8df7c5f29a605aa84c82b996e09
Mohamed Thaiseer <mohamed.thaiseer@gmail.com>
2026-05-01 23:34:38 +0300

    add

--- 001_Basics/springboot-jpa-caching/src/main/java/com/example/springboot_jpa_caching/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot_jpa_caching.repository;
-
-import com.example.springboot_jpa_caching.entity.Author;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- 002_Microservices_with_cloud/RestfulMicroservices/src/main/java/com/example/restfulMicroservices/author/NoAuthorCreatedException.java
+++
@@ -1,8 +0,0 @@
-package com.example.restfulMicroservices.author;
-
-public class NoAuthorCreatedException extends RuntimeException{
-
-    public NoAuthorCreatedException(String message){
-        super(message);
-    }
-}
